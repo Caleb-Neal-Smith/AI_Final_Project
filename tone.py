0054dd0639af8843961c01a25d571f36a9b76ee5
Noah Herron <noahherron2002@gmail.com>
2024-11-04 13:02:07 -0600

    adding colored text

--- tone.py
+++ tone.py
@@ -14,3 +14,19 @@ response = requests.post(
 )
 
 pprint(response.json())
+print("\033[1;31;40m '{content}' \033[0m")
+
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+print(style.BLUE + "Hello, World!")
+print("\033[0m")
